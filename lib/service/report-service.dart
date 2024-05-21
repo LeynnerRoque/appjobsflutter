c68b095d29ee23f5af2a19bdc04fc7491506c505
Leynner Roque <151456582+LeynnerRoque@users.noreply.github.com>
2024-05-21 14:41:38 -0300

    Update report-service.dart

--- lib/service/report-service.dart
+++ lib/service/report-service.dart
@@ -1,7 +1,7 @@
 import 'package:http/http.dart' as http;
 
 class ReportService {
-  final url = 'http://192.168.68.30:8080/report/';
+  final url = 'http://xxx.xxx.xx.xx:8080/report/';
 
   Future<http.Response> reportVisualizer(name) {
     return http.get(Uri.parse(url + 'visualizer/'+name));
